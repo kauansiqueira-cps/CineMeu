8651d77761a55eac04cd30859a2a38ba9ad72dce
kauansiqueira-cps <kauan.siqueira@etec.sp.gov.br>
2024-07-31 22:45:51 -0300

    branch com o código da aula do dia Wed, Jul 31, 2024 10:45:50 PM

---
+++ lib/componentes/filme_item.dart
@@ -0,0 +1,28 @@
+import 'package:cinemeu/entidade/filme.dart';
+import 'package:flutter/material.dart';
+
+class FilmeItem extends StatelessWidget {
+  final Filme filme;
+
+  const FilmeItem({super.key, required this.filme});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(
+        left: 4,
+        right: 4,
+        top: 4,
+      ),
+      child: Material(
+        elevation: 5,
+        child: Row(
+          children: [
+            //Image.network(filme.poster),
+            Text(filme.titulo),
+          ],
+        ),
+      ),
+    );
+  }
+}
